c98476bfc4b46f03684948ad6172f15a0faa40ed
claudenirmf <claudenirmf@gmail.com>
2016-03-15 05:28:11 -0300

    Commit

--- br.ufes.inf.nemo.vp_ontouml_plugin/src/br/ufes/inf/nemo/vp_ontouml_plugin/ufo_a/CollectiveClass.java
+++
@@ -1,8 +0,0 @@
-package br.ufes.inf.nemo.vp_ontouml_plugin.ufo_a;
-
-public interface CollectiveClass extends SubstantialClass {
-	
-	public boolean isExtensinable();
-	public void setIsExtensinable(boolean value);
-
-}
--- br.ufes.inf.nemo.vp_ontouml_plugin/src/br/ufes/inf/nemo/vp_ontouml_plugin/ufo_a/MixinClass.java
+++
@@ -1,5 +0,0 @@
-package br.ufes.inf.nemo.vp_ontouml_plugin.ufo_a;
-
-public interface MixinClass extends EndurantClass {
-
-}
